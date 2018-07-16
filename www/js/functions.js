b50309bf5a2379bd1d06454369a70b8d7b8418c6
ChaozzJ <direccion@epicalesoft.com>
2018-07-16 12:36:20 -0600

    Camera Description

--- www/js/functions.js
+++ www/js/functions.js
@@ -7,8 +7,10 @@ $(document).ready(function()
     $.each(result.posts, function(i, field){
        var title=field.title;
        var link=field.url;
+       var mid=field.id;
        var imgpost=field.thumbnail;
-       $("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"cordova.InAppBrowser.open('" + link + "','_system'); return false; \" > <h2>"+title+"</h2></a></li>");
+       $("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"navigatepage('getpost.html?id="+ mid +"')\" > <h2>"+title+"</h2></a></li>");
+       //$("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"cordova.InAppBrowser.open('" + link + "','_system'); return false; \" > <h2>"+title+"</h2></a></li>");
     });
    }).fail(function(jqXHR) {
     if (jqXHR.status == 404) {
--- www/js/getanalsem.js
+++ www/js/getanalsem.js
@@ -7,14 +7,16 @@ $(document).ready(function()
     $.each(result.posts, function(i, field){
        var title=field.title;
        var link=field.url;
+       var mid=field.id;
        var imgpost=field.thumbnail;
-       $("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"cordova.InAppBrowser.open('" + link + "','_system'); return false; \" > <h2>"+title+"</h2></a></li>");
+       $("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"navigatepage('getpost.html?id="+ mid +"')\" > <h2>"+title+"</h2></a></li>");
+       //$("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"cordova.InAppBrowser.open('" + link + "','_system'); return false; \" > <h2>"+title+"</h2></a></li>");
     });
    }).fail(function(jqXHR) {
     if (jqXHR.status == 404) {
        $("#posts").append("<li><h1>Error 404</h1></li>");
     } else {
-        $("#posts").append("<li><h1>Error al obtener noticias, compruebe su conexión.</h1></li>");
+        $("#posts").append("<li><h1>Error al obtener Condiciones Registradas En La Semana, compruebe su conexión.</h1></li>");
     }
 });;
 });
